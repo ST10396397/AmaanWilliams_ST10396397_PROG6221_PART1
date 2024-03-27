b0dfaa21d4daac0804e8919603f4a698b5eb3a17
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2024-03-27 10:44:34 +0200

    Add project files.

---
+++ AmaanWilliams_ST10396397_PROG6221_PART1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nOptions:");
+        Console.WriteLine("1. Enter recipe details");
+        Console.WriteLine("2. Display recipe");
+        Console.WriteLine("3. Scale recipe");
+        Console.WriteLine("4. Reset quantities");
+        Console.WriteLine("5. Clear recipe");
+        Console.WriteLine("6. Exit");
+    }
+
+  
+}
