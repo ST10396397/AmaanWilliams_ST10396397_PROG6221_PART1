5f662a0574e61a6e79a1f0c0a3907b73a2916dec
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2024-04-03 20:46:46 +0200

    Trial for program class

--- AmaanWilliams_ST10396397_PROG6221_PART1/Program.cs
+++ AmaanWilliams_ST10396397_PROG6221_PART1/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AmaanWilliams_ST10396397_PROG6221_PART1;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.CompilerServices;
+
 class Program
 {
     static void Main(string[] args)
@@ -11,7 +15,46 @@ class Program
         Console.WriteLine("4. Reset quantities");
         Console.WriteLine("5. Clear recipe");
         Console.WriteLine("6. Exit");
-    }
 
-  
+        Console.WriteLine("Enter your choice: ");
+        int choice = ReadChoice();
+
+        if(Choice == 1)
+        {
+            Recipe.EnterRecipeDetails();
+        }
+        else if (choice == 2)
+        {
+            Recipe.DisplayRecipe();
+        }
+        else if (choice == 3)
+        {
+            Console.Write("Enter scaling (0.5, 2 or 3) ");
+            double factor = ReadSclaingFactor();
+            Recipe.ScaleRecipe(factor);
+            Recipe.DisplayRecipe();
+        }
+        else if(choice == 4)
+        {
+            Recipe.ResetQuantites();
+            Recipe.DisplayRecipe();
+        }
+        else if (choice == 5)
+        {
+            Recipe.ClearRecipe();
+        }
+        else if (choice ==6)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice. Please enter a number between 1 and 6");
+        }      
+    }
 }
+
+
+
+
+
--- AmaanWilliams_ST10396397_PROG6221_PART1/Recipe.cs
+++ AmaanWilliams_ST10396397_PROG6221_PART1/Recipe.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace AmaanWilliams_ST10396397_PROG6221_PART1
 {
-    internal class Recipe
+    class Recipe
     {
+        private string[] ingredients;
+        private string[] steps;
+
+        public void EnterRecipeDetails()
+        {
+
+        }
+
+        public void DisplayRecipe()
+        {
+
+        }
+
+        public void ScaleRecipe(double factor)
+        {
+
+        }
+
+        public void ResetQuantities()
+        {
+
+        }
+
+        public void ClearRecipe()
+        {
+
+        }
+
+        private int ReadIntegerInput()
+        { 
+        }
+
     }
 }
